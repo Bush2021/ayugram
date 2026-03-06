32e6507bccb84894a6ce8d719689e16676cb918c
Bush2021 <79072750+Bush2021@users.noreply.github.com>
2026-03-06 16:32:10 -0500

    fix: make it build

--- Telegram/SourceFiles/ayu/features/forward/ayu_sync.cpp
+++ Telegram/SourceFiles/ayu/features/forward/ayu_sync.cpp
@@ -271,7 +271,7 @@ void sendDocumentSync(not_null<Main::Session*> session,
 	crl::on_main([=, lst = std::move(group.list), caption = std::move(caption)]() mutable
 	{
 		auto size = lst.files.size();
-		session->api().sendFiles(std::move(lst), type, std::move(caption), size > 1 ? groupId : nullptr, action);
+		session->api().sendFiles(std::move(lst), type, size > 1 ? groupId : nullptr, action);
 	});
 
 	waitForMsgSync(session, action);
