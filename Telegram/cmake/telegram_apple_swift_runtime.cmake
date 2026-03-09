0a092ca0d43a9668abc5505562715af92e799898
Bush2021 <79072750+Bush2021@users.noreply.github.com>
2026-03-09 13:53:39 -0400

    Revert "Temporary fix for macOS build"
    This reverts commit 90588765b7703bf464b8dd2c797d90320196502c because it
    is fixed on upstream.

--- Telegram/cmake/telegram_apple_swift_runtime.cmake
+++ Telegram/cmake/telegram_apple_swift_runtime.cmake
@@ -9,31 +9,8 @@ function(telegram_add_apple_swift_runtime target_name)
         return()
     endif()
 
-    execute_process(
-        COMMAND xcrun --find swiftc
-        OUTPUT_VARIABLE _swift_compiler_path
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        ERROR_QUIET
-    )
-    if (NOT _swift_compiler_path)
-        set(_swift_compiler_path "${CMAKE_Swift_COMPILER}")
-    endif()
-
-    get_filename_component(_swift_compiler_dir "${_swift_compiler_path}" DIRECTORY)
-    get_filename_component(_swift_toolchain_dir "${_swift_compiler_dir}/../.." REALPATH)
-    set(_swift_runtime_dir "${_swift_toolchain_dir}/usr/lib/swift")
-    set(_swift_platform_runtime_dir "${_swift_runtime_dir}/macosx")
-
-    target_link_directories(${target_name}
-    PRIVATE
-        "${_swift_platform_runtime_dir}"
-        "${_swift_runtime_dir}"
-    )
-
     target_link_options(${target_name}
     PRIVATE
-        "-L${_swift_platform_runtime_dir}"
-        "-L${_swift_runtime_dir}"
         "-Wl,-rpath,/usr/lib/swift"
         "-Wl,-rpath,@executable_path/../Frameworks"
     )
