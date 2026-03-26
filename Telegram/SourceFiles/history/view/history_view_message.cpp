060c49125ab7df38aeb59e1f6da668657387d5ea
Ivan Romanov <drizt72@zoho.eu>
2026-03-26 19:55:33 +0500

    feat: option to hide premium statuses
    Option allows to hide premium statuses in chat list, in group chats and above contact chat.

--- Telegram/SourceFiles/history/view/history_view_message.cpp
+++ Telegram/SourceFiles/history/view/history_view_message.cpp
@@ -1738,7 +1738,9 @@ void Message::paintFromName(
 		}
 		return &info->nameText();
 	}();
-	const auto statusWidth = _fromNameStatus
+	const auto &settings = AyuSettings::getInstance();
+	const auto hidePremiumStatuses = settings.hidePremiumStatuses();
+	const auto statusWidth = _fromNameStatus && !hidePremiumStatuses
 		? st::dialogsPremiumIcon.icon.width()
 		: 0;
 	if (statusWidth && availableWidth > statusWidth) {
@@ -1796,7 +1798,7 @@ void Message::paintFromName(
 		.elisionLines = 1,
 	});
 	const auto skipWidth = nameText->maxWidth()
-		+ (_fromNameStatus
+		+ (_fromNameStatus && !hidePremiumStatuses
 			? (st::dialogsPremiumIcon.icon.width()
 				+ st::msgServiceFont->spacew)
 			: 0)
